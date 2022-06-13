878cd4d73a7e3faaf797b6c694458a1b65842380
ArieivSakul <sakul_897@outlook.com>
2022-06-13 19:14:57 -0300

    update
    update no main.dart

--- lib/pages/main.dart
+++ lib/pages/main.dart
@@ -1,8 +1,17 @@
 import 'package:flutter/material.dart';
 
+import '../firebase_options.dart';
 import 'login.page.dart';
 
-void main() => runApp(const MyApp());
+import 'package:firebase_core/firebase_core.dart';
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  runApp(const MyApp());
+}
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
