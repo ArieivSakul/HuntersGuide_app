523afdbcde36043d38dc1ebe2e1f77196475e52e
ArieivSakul <sakul_897@outlook.com>
2022-06-13 21:29:49 -0300

    reajustes
    reajustando paginas e realocando o main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
-import '../firebase_options.dart';
-import 'login.page.dart';
+import 'firebase_options.dart';
+import 'pages/login.page.dart';
 
 import 'package:firebase_core/firebase_core.dart';
 
